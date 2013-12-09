80926871b53b65c42fcdce30dcfec6dba2f90452
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-12-08 22:32:04 -0600

    Added EXPM package file.

---
+++ package.exs
@@ -0,0 +1,8 @@
+Expm.Package.new(name: "lfe-rackspace",
+                 description: "Pure LFE language bindings for the Rackspace Cloud",
+                 version: "0.0.1-alpha",
+                 keywords: ["LFE", "Lisp", "Rackspace", "OpenStack", "Cloud",
+                            "Library", "API", "Compute"],
+                 maintainers: [[name: "Duncan McGreggor",
+                                email: "oubiwann@cogitat.io"]],
+                 repositories: [[github: "oubiwann/lfe-rackspace"]])
